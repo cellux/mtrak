cc62d29e064a3193ea4615359c43fba67d5c14e7
Ruzsa Balazs <ruzsa.balazs@gmail.com>
2024-10-06 15:24:50 +0200

    change number of pattern tracks with "tracks" command

--- commands.go
+++ commands.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"slices"
 	"strconv"
 	"strings"
@@ -29,6 +30,10 @@ func (m *model) ExecuteCommand(command string) {
 				m.SetError(err)
 				return
 			}
+			if bpm < 1 {
+				m.SetError(fmt.Errorf("invalid BPM: %d", bpm))
+				return
+			}
 			m.song.BPM = bpm
 		}
 	case "lpb":
@@ -38,6 +43,10 @@ func (m *model) ExecuteCommand(command string) {
 				m.SetError(err)
 				return
 			}
+			if lpb < 1 {
+				m.SetError(fmt.Errorf("invalid LPB: %d", lpb))
+				return
+			}
 			m.song.LPB = lpb
 		}
 	case "tpl":
@@ -45,6 +54,10 @@ func (m *model) ExecuteCommand(command string) {
 			tpl, err := strconv.Atoi(items[1])
 			if err != nil {
 				m.SetError(err)
+				return
+			}
+			if tpl < 1 {
+				m.SetError(fmt.Errorf("invalid TPL: %d", tpl))
 				return
 			}
 			m.song.TPL = tpl
@@ -60,6 +73,10 @@ func (m *model) ExecuteCommand(command string) {
 			if rows == len(p) {
 				return
 			}
+			if rows < 1 {
+				m.SetError(fmt.Errorf("invalid row count: %d", rows))
+				return
+			}
 			clone := p.clone()
 			if rows < len(clone) {
 				clone = clone[:rows]
@@ -68,6 +85,44 @@ func (m *model) ExecuteCommand(command string) {
 				trackCount := len(p[0])
 				for i := 0; i < rows-len(p); i++ {
 					clone = append(clone, make([]MidiMessage, trackCount))
+				}
+			}
+			m.submitAction(
+				func() {
+					m.ReplaceEditPattern(clone)
+				},
+				func() {
+					m.ReplaceEditPattern(p)
+				},
+			)
+		}
+	case "tracks":
+		if len(items) > 1 {
+			tracks, err := strconv.Atoi(items[1])
+			if err != nil {
+				m.SetError(err)
+				return
+			}
+			p := m.song.Patterns[m.editPattern]
+			currentTrackCount := len(p[0])
+			if tracks == currentTrackCount {
+				return
+			}
+			if tracks < 1 {
+				m.SetError(fmt.Errorf("invalid track count: %d", tracks))
+				return
+			}
+			clone := p.clone()
+			if tracks < currentTrackCount {
+				for i := 0; i < len(clone); i++ {
+					clone[i] = clone[i][:tracks]
+				}
+			} else {
+				for i := 0; i < len(clone); i++ {
+					clone[i] = slices.Grow(clone[i], tracks-currentTrackCount)
+					for j := 0; j < tracks-currentTrackCount; j++ {
+						clone[i] = append(clone[i], MidiMessage{})
+					}
 				}
 			}
 			m.submitAction(
